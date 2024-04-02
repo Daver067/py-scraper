b7f1e489c36a2637bbc1281ed59621748a91a153
Dave Diemert <davediemert_88@hotmail.com>
2024-04-02 18:21:44 +0700

    cleaned up into functions

--- app.py
+++ app.py
@@ -1,39 +1,38 @@
+import requests
+import re
+
 def main():
-    import requests
-    from bs4 import BeautifulSoup
-    import re
-    regex = r'"mailto:\w{1,50}@\w{1,50}\.[a-z]{2,5}"'
-    regex2 = r'\w{1,50}@\w{1,50}\.(?:ca|com)'
-    URL = "https://rockyridgedental.com"
+    URL = "https://www.moveoutcleaningserviceaustin.com"
     page = requests.get(URL, timeout=5)
+    get_phone_numbers(page)
+    get_email_address(page)
+    return 0
 
-
+def get_phone_numbers(page):
     phone_regex = r"(?:\+?\d{1}\s?)?[(]?\d{3}[)]?[(\s)?.-]\d{3}[\s.-]\d{4}"
     numbers = re.findall(phone_regex, page.text)
     if len(numbers) == 0:
         print('didn\'t find any phone numbers')
+        return None
     else:
         sorted_phone_numbers = []
-        
         for phone_number in numbers:
-            if phone_number in sorted_phone_numbers:
+            if phone_number in sorted_phone_numbers or phone_number == "(999) 999-9999":
                 continue
             sorted_phone_numbers.append(phone_number)
         for num in sorted_phone_numbers:
             print(num)
-    #soup = BeautifulSoup(page.content, "html.parser")
+        return sorted_phone_numbers
 
-    #emails = soup.find_all(href=re.compile('([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(.[A-Z|a-z]{2,})+')) or None
-    #if emails == None:
-    #    emails = soup.find_all(href=re.compile('mailto:')) or None
-    #if emails == None:
-    #    print('no emails found')
-    #else:
+
+def get_email_address(page):
+    regex = r'"mailto:\w{1,50}@\w{1,50}\.[a-z]{2,5}"'
+    regex2 = r'\w{1,50}@\w{1,50}\.(?:ca|com)'
     emails = re.findall(regex, page.text, re.IGNORECASE)
     if len(emails) == 0:
         emails = re.findall(regex2, page.text, re.IGNORECASE)
         if len(emails) == 0:
-            print('didn\'t find any')
+            print('didn\'t find any emails')
             return 0
         sorted_emails = []
         for email in emails:
@@ -55,5 +54,4 @@ def main():
             
         for email in sorted_emails:
             print(email)
-
 main()
