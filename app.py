cb3b0f3a9e54c9861441ffea6167490d2865e804
Dave Diemert <davediemert_88@hotmail.com>
2024-07-07 11:48:32 -0600

    added a csv reader/writer

--- app.py
+++ app.py
@@ -2,18 +2,55 @@ import requests
 import re
 
 def main():
-    URL = "https://www.moveoutcleaningserviceaustin.com"
+    if True:
+        read_file('website_list.csv')
+        return
+    URL = "https://www.cleanusa.com"
     page = requests.get(URL, timeout=5)
-    get_phone_numbers(page)
-    get_email_address(page)
+    if page.status_code != 200:
+        print('page inaccessible')
+        return 0
+    #get_phone_numbers(page)
+    #get_email_address(page)
     return 0
+
+def read_file(filename):
+    with open(filename, 'r') as readFile, open(filename.replace('.csv', '_new.csv'), 'w') as writeFile:
+        for line in readFile:
+            line = line.replace("\n", "")
+            parts = line.split(";")
+            if parts[0] == "Website": # Need to figure out the header to actually use here
+                writeFile.write(";".join(parts) + '\n')
+                continue
+            page = requests.get(parts[0], timeout=5)
+
+            # If theres an error reaching to page
+            if page.status_code != 200:
+                parts[1] = ('page inaccessible')
+                parts[2] = ('page inaccessible')
+                parts[3] = ('page inaccessible')
+                writeFile.write(";".join(parts) + "\n")
+                continue
+
+            # Update the email part of th csv
+            for email_address in get_email_address(page):
+                parts[1] += str(email_address) + ","
+
+            # Update the phone number section of the csv
+            for index, phone_num in enumerate(get_phone_numbers(page)):
+                if index == 0:
+                    parts[2] += str(phone_num)
+                else:
+                    parts[2] += "," + str(phone_num)
+            writeFile.write(";".join(parts) + "\n")
+
+
 
 def get_phone_numbers(page):
     phone_regex = r"(?:\+?\d{1}\s?)?[(]?\d{3}[)]?[(\s)?.-]\d{3}[\s.-]\d{4}"
     numbers = re.findall(phone_regex, page.text)
-    if len(numbers) == 0:
-        print('didn\'t find any phone numbers')
-        return None
+    if len(numbers) == 0 or len(numbers) > 10:
+        return ['no numbers found']
     else:
         sorted_phone_numbers = []
         for phone_number in numbers:
@@ -33,7 +70,7 @@ def get_email_address(page):
         emails = re.findall(regex2, page.text, re.IGNORECASE)
         if len(emails) == 0:
             print('didn\'t find any emails')
-            return 0
+            return ['no emails found']
         sorted_emails = []
         for email in emails:
             if email in sorted_emails:
@@ -54,4 +91,5 @@ def get_email_address(page):
             
         for email in sorted_emails:
             print(email)
+        return sorted_emails
 main()
